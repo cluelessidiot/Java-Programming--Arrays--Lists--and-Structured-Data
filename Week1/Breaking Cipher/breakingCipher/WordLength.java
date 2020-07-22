6e8d55e405a40a2183114ea6da07702de4cc8c24
AATHIL.TA <cluelessidiot@users.noreply.github.com>
2020-07-22 22:09:28 +0530

    dataset added

---
+++ Week1/Breaking Cipher/breakingCipher/WordLength.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of WordLength here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import edu.duke.*;
+public class WordLength {
+
+ void  testCountWordLengths(){
+    FileResource fr= new FileResource();
+    String fileContent=fr.asString();
+    System.out.println(fileContent);
+    
+    
+    }   
+    
+    
+}
