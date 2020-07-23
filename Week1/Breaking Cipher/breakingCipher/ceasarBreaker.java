52b6dee8678d7deaee9a1c53ec3d2348e91acfe0
AATHIL.TA <cluelessidiot@users.noreply.github.com>
2020-07-23 17:59:34 +0530

    2key caesar cipher added

--- Week1/Breaking Cipher/breakingCipher/ceasarBreaker.java
+++ Week1/Breaking Cipher/breakingCipher/ceasarBreaker.java
@@ -28,6 +28,14 @@ for(int i=0;i<string.length();i++)
     }
     return maxIndex;
     }
+    
+   String halfOfString (String str,int start){
+    String newstr="";
+    for(int i=start;i<str.length();i=i+2){
+    newstr=newstr+str.charAt(i);
+    }
+    return newstr;
+    } 
    String decrypt(String msg){
     cesarCipher cc=new cesarCipher();
     int [] freq=new int[26];
@@ -36,9 +44,32 @@ for(int i=0;i<string.length();i++)
     int dkey=maxKey-4;
     if(maxKey<4)
        dkey=26-(4-maxKey);
+       
+    System.out.println("key;"+dkey);   
     return cc.encrypt(msg,26-dkey);
     
     
+    } 
+       String decryptTwoKeys(String msg){
+           String p1=halfOfString(msg,0);
+            String p2=halfOfString(msg,1);
+             System.out.println(decrypt(p1));
+             System.out.println(decrypt(p2));
+              String s1=decrypt(p1);
+              String s2=decrypt(p2);
+              String newstr="";
+        for(int i=0;i<s1.length();i++){
+             if(i<s2.length())
+               {
+                newstr=newstr+s1.charAt(i);
+                newstr=newstr+s2.charAt(i);
+                }
+               else
+                 newstr=newstr+s1.charAt(i);
+                 
+    }
+    System.out.println("Ans :"+newstr);
+    return newstr;
     } 
   void testDecrypt(){
    FileResource fr= new FileResource();  
