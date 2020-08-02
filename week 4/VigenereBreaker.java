9579bfdea3cefbc7f6f31c2c51359e55f7eb7d1b
AATHIL.TA <cluelessidiot@users.noreply.github.com>
2020-08-02 11:45:48 +0530

    VigenereBreaker on unknown key final commit

--- week 4/VigenereBreaker.java
+++ week 4/VigenereBreaker.java
@@ -1,6 +1,6 @@
 import java.util.*;
 import edu.duke.*;
-
+import java.io.StringReader;  
 public class VigenereBreaker {
     public String sliceString(String message, int whichSlice, int totalSlices) {
         String a="";
@@ -21,7 +21,13 @@ public class VigenereBreaker {
     }
 
     public void breakVigenere () {
-        //WRITE YOUR CODE HERE
+        FileResource fr = new FileResource ();
+        String msg = fr.asString();
+        fr = new FileResource ();
+        HashSet <String> dict = readDictionary(fr);
+        String decr = breakForLanguage(msg,dict);
+       // String s =decr.Split(new [] { '\r', '\n' }).FirstOrDefault();
+        System.out.println(decr.substring(0, 100));
     }
     HashSet readDictionary(FileResource fr){
     HashSet <String> dict = new HashSet <String> ();
@@ -31,11 +37,31 @@ public class VigenereBreaker {
     }
     return dict;
     }
+    String breakForLanguage(String encrypted,HashSet<String> dict){
+    int maxKeyLength = 1;
+    int wordCount = 0;
+    String origMessage="";
+    int [] q;
+    for(int i=1;i<=100;i++){
+    int [] k  = tryKeyLength(encrypted,i,'e');
+    VigenereCipher vc = new VigenereCipher(k);
+    String decrypt = vc.decrypt(encrypted);
+    if(countWords(decrypt,dict)>wordCount){
+      maxKeyLength = i;
+      wordCount = countWords(decrypt,dict);
+      origMessage = decrypt;
+    }
+    
+    }
+    int [] k=tryKeyLength(encrypted,maxKeyLength,'e');
+    System.out.println(wordCount+"  WC  "+maxKeyLength+"  "+k[0]+" "+k[1]);
+    return  origMessage;
+    }
      int countWords(String message,HashSet<String> dict){
         String [] split = message.split("\\W+");
         int ct = 0;
         for(int i=0;i<split.length;i++){
-          if(dict.contains(split[i]))
+          if(dict.contains(split[i].toLowerCase()))
                ct++;        
         
         }
